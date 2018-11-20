5bce9f4c2eeb31d4559e34f59d75f0bcc7b95bd9
florent champigny <florent.champigny@backelite.com>
2018-11-20 11:23:11 +0100

    added sample

---
+++ app/src/main/java/com/github/florent37/motionshapeofview/MainActivityMovie.kt
@@ -0,0 +1,14 @@
+package com.github.florent37.motionshapeofview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_arc_list.*
+
+class MainActivityMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cinema)
+    }
+}
